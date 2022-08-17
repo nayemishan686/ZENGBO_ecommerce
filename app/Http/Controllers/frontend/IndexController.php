bc377eed3b93b0f6eefb8dc0544eb213a8731082
Nayem Ishan <nayemishan377@gmail.com>
2022-08-18 00:45:29 +0600

    Banner Product show done

--- app/Http/Controllers/frontend/IndexController.php
+++ app/Http/Controllers/frontend/IndexController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class IndexController extends Controller
 {
     public function index(){
         $category = Category::all();
-        return view('frontend.index', compact('category'));
+        $bannerProduct = Product::where('product_slider',1)->first();
+        return view('frontend.index', compact('category','bannerProduct'));
     }
 }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,13 +6,17 @@
             <div class="banner_background" style="background-image:url(images/banner_background.jpg)"></div>
             <div class="container fill_height">
                 <div class="row fill_height">
-                    <div class="banner_product_image"><img src="{{ asset('frontend') }}/images/banner_product.png"
-                            alt=""></div>
+                    <div class="banner_product_image"><img src="{{ asset('files/products/'.$bannerProduct->thumbnail) }}"
+                            alt="{{$bannerProduct->name}}"></div>
                     <div class="col-lg-5 offset-lg-4 fill_height">
                         <div class="banner_content">
-                            <h1 class="banner_text">new era of smartphones</h1>
-                            <div class="banner_price"><span>$530</span>$460</div>
-                            <div class="banner_product_name">Apple Iphone 6s</div>
+                            <h1 class="banner_text">{{$bannerProduct->name}}</h1>
+                            @if ($bannerProduct->discount_price == null)
+                            <div class="banner_price">${{$bannerProduct->selling_price}}</div>
+                               @else 
+                            <div class="banner_price"><span>${{$bannerProduct->selling_price}}</span>${{$bannerProduct->discount_price}}</div>
+                            @endif
+                            <div class="banner_product_name">{{$bannerProduct->brand->brand_name}}</div>
                             <div class="button banner_button"><a href="#">Shop Now</a></div>
                         </div>
                     </div>
